8f52e1acb0a6daa33ab60f566a4cb2a9bc033507
萬俊瑋 <wei840222@outlook.com>
2017-11-12 16:17:55 +0800

    TEST(ParserTest, createTerm_Var) is pass

--- parser.h
+++ parser.h
@@ -1,20 +1,17 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "scanner.h"
 #include "atom.h"
 #include "number.h"
 #include "variable.h"
 #include "struct.h"
 #include "list.h"
-#include "scanner.h"
 #include <string>
 #include <vector>
 
 using std::string;
 using std::vector;
-
-#include <iostream>
-using std::cout;
 
 class Parser
 {
@@ -22,65 +19,19 @@ public:
   Parser(Scanner scanner) : _scanner(scanner) {}
   Term *createTerm()
   {
-    int token = _scanner.nextToken();
-    _currentToken = token;
-    if (token == VAR)
+    _scanner.nextToken();
+    if (_scanner.currentToken()->type() == Token::VAR)
     {
-      return new Variable(symtable[_scanner.tokenValue()].first);
+      return new Variable(_scanner.currentToken()->value());
     }
-    else if (token == NUMBER)
+    else
     {
-      return new Number(_scanner.tokenValue());
+      return nullptr;
     }
-    else if (token == ATOM || token == ATOMSC)
-    {
-      Atom *atom = new Atom(symtable[_scanner.tokenValue()].first);
-      if (_scanner.currentChar() == '(')
-      {
-        _scanner.nextToken();
-        vector<Term *> terms = getArgs();
-        if (_currentToken == ')')
-        {
-          return new Struct(*atom, terms);
-        }
-      }
-      else
-        return atom;
-    }
-    else if (token == '[')
-    {
-      vector<Term *> terms = getArgs();
-      if (_currentToken == ')')
-        throw(std::string("unexpected token"));
-      if (_currentToken == ']' || _currentToken == 0)
-      {
-        return new List(terms);
-      }
-    }
-    return nullptr;
-  }
-
-  vector<Term *> getArgs()
-  {
-    Term *term = createTerm();
-    vector<Term *> args = {};
-    if (term)
-    {
-      args.push_back(term);
-    }
-    if ((_currentToken == ']' || _currentToken == ')') && term == nullptr)
-    {
-      return args;
-    }
-    while ((_currentToken = _scanner.nextToken()) == ',')
-    {
-      args.push_back(createTerm());
-    }
-    return args;
   }
 
 private:
   Scanner _scanner;
-  int _currentToken;
 };
+
 #endif
