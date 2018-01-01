af8f842ef598cf62509aaa064b955570f0a73bcb
萬俊瑋 <wei840222@outlook.com>
2018-01-01 19:20:16 +0800

    TEST(Shell, disjunctionMatching3) is pass

--- expression.h
+++ expression.h
@@ -333,25 +333,20 @@ TEST(Shell, disjunctionMatching2)
   }
 }
 
-  // TEST(Shell, disjunctionMatching3)
-  // {
-  //   Scanner s("X=1; X=1, Y=2.");
-  //   Parser p(s);
-  //   try
-  //   {
-  //     p.buildExpression();
-
-  //     /**
-  //      *  maybe your implementation here.
-  //      */
-
-  //     ASSERT_EQ("X = 1; X = 1, Y = 2.", result->toString());
-  //   }
-  //   catch (std::string &msg)
-  //   {
-  //     FAIL() << msg;
-  //   }
-  // }
+TEST(Shell, disjunctionMatching3)
+{
+  Scanner s("X=1; X=1, Y=2.");
+  Parser p(s);
+  try
+  {
+    string result = p.buildExpression()->getResult() + '.';
+    ASSERT_EQ("X = 1; X = 1, Y = 2.", result);
+  }
+  catch (std::string &msg)
+  {
+    FAIL() << msg;
+  }
+}
 
   // TEST(Shell, disjunctionMatching4)
   // {
